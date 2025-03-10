da167438865cb87acaae983d58b929fda2e303c8
Winnie Regan <reganwinnifred@gmail.com>
2025-03-10 16:14:08 -0600

    The requirements file was a lie

--- motor_venv/bin/odrive_demo.py
+++ motor_venv/bin/odrive_demo.py
@@ -16,17 +16,17 @@ my_drive = odrive.find_any()
 
 # Calibrate motor and wait for it to finish
 print("starting calibration...")
-my_drive.axis0.requested_state = AxisState.FULL_CALIBRATION_SEQUENCE
-while AxisState(my_drive.axis0.current_state) != AxisState.IDLE:
+my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while my_drive.axis0.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
 
-my_drive.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
+my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
 # To read a value, simply read the property
 print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
 
 # Or to change a value, just assign to the property
-my_drive.axis0.controller.pos_setpoint = 3.14
+my_drive.axis0.controller.input_pos = 3.14
 print("Position setpoint is " + str(my_drive.axis0.controller.pos_setpoint))
 
 # And this is how function calls are done:
@@ -38,7 +38,7 @@ t0 = time.monotonic()
 while True:
     setpoint = 4.0 * math.sin((time.monotonic() - t0)*2)
     print("goto " + str(int(setpoint)))
-    my_drive.axis0.controller.pos_setpoint = setpoint
+    my_drive.axis0.controller.input_pos = setpoint
     time.sleep(0.01)
 
 # Some more things you can try:

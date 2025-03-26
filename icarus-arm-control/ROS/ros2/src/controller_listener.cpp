384589cef1ca1822170d0945dae63cbb9a558b95
Winnie <reganwinnifred@gmail.com>
2025-03-25 22:16:10 -0600

    TEST ADD NEW LISTENER NODE

---
+++ icarus-arm-control/ROS/ros2/src/controller_listener.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("Here will be the controller listener.\nThis node will do gravity correction and control law calculations.\n");
+  return 0;
+}

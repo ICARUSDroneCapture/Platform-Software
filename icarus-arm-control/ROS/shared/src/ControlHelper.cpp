3d148f24bf2c747d83b67229b8b15052000810d0
Winnie <reganwinnifred@gmail.com>
2025-03-28 14:35:10 -0600

    Test add new control helper class

---
+++ icarus-arm-control/ROS/shared/src/ControlHelper.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file ControlHelper.cpp
+ *
+ * @brief This file contains all the functionality code for matrix math
+ *
+ */
+
+#include "ControlHelper.hpp"
+
+void ControlHelper::someFunction()
+{
+    std::out << "Failed to get interface index" << std::endl;
+}

8f9aee2d67acd98d5621ec9a559a06de4b2be0c1
Winnie <reganwinnifred@gmail.com>
2025-03-28 17:39:37 -0600

    Test use address

--- icarus-arm-control/ROS/ros2/src/controller.cpp
+++ icarus-arm-control/ROS/ros2/src/controller.cpp
@@ -21,9 +21,9 @@ void Controller::step()
 
   // cbPIMU(&pimu);
 
-  cbIMU(imu);
+  // cbIMU(imu);
 
-  // cbIMU(&imu);
+  cbIMU(&imu);
 
   RCLCPP_INFO(rclcpp::get_logger("data"),"\t\tLinear Velocity: [%f; %f; %f]\n", linear_velocity_S_x, linear_velocity_S_y, linear_velocity_S_z);
   

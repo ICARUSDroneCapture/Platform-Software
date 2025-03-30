beee253d93ea9fb4f8c22c3043a534dd6a9a60ce
Winnie <reganwinnifred@gmail.com>
2025-03-30 14:37:36 -0600

    test timestamp

--- icarus-arm-control/ROS/ros2/src/controller.cpp
+++ icarus-arm-control/ROS/ros2/src/controller.cpp
@@ -186,7 +186,7 @@ void Controller::cbIMU(const  sensor_msgs::msg::Imu &imu)
     // Update timesteps array
     // insert_front(ts_ptr, timestep_store, imu.header.stamp.sec);
     // *(ts_ptr+i) = imu.header.stamp.sec;
-    *ts_ptr = imu.header.stamp.sec;
+    *ts_ptr = &imu.header.stamp.sec;
     RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\tTimestamp: [%f]\n", *ts_ptr);
     
 

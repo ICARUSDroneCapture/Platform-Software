1231b38af84fe16922754b448e6270d8fbbdb091
Winnie <reganwinnifred@gmail.com>
2025-03-30 21:05:39 -0600

    print away

--- icarus-arm-control/ROS/ros2/src/controller.cpp
+++ icarus-arm-control/ROS/ros2/src/controller.cpp
@@ -74,10 +74,12 @@ void Controller::plot()
   // matplot::ylim({-20, 0});
   // matplot::hold(matplot::on);
 
-  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ttime: [%f]\n", plot_ts.begin());
-  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ta_x: [%f]\n", plot_a_x.begin());
-  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ta_y: [%f]\n", plot_a_y.begin());
-  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ta_z: [%f]\n", plot_a_z.begin());
+  RCLCPP_INFO(rclcpp::get_logger("data"),"\t\tLinear Acceleration: [%f; %f; %f]\n", linear_acceleration_S_x, linear_acceleration_S_y, linear_acceleration_S_z);
+
+  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ttime: [%f, %f]\n", plot_ts.begin(), plot_ts[1]);
+  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ta_x: [%f, %f]\n", plot_a_x.begin(), plot_a_x[1]);
+  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ta_y: [%f, %f]\n", plot_a_y.begin(), plot_a_y[1]);
+  RCLCPP_INFO(rclcpp::get_logger("debug"),"\t\ta_z: [%f, %f]\n", plot_a_z.begin(), plot_a_z[1]);
 
   auto ax1 = matplot::nexttile();
   auto ax2 = matplot::nexttile();

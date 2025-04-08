1087fbf669272106522ea66c68c18201bd38819f
Winnie Regan <reganwinnifred@gmail.com>
2025-04-08 17:06:10 -0600

    Added integrator node.

---
+++ icarus-arm-control/include/imu_pimu.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file controller.hpp
+ *
+ * @brief This file contains the controller class (subclass of Node) and all member functions
+ *
+ */
+
+ #ifndef IMU_PIMU_LISTENER_HPP
+ #define IMU_PIMU_LISTENER_HPP
+ #include "sensor_msgs/msg/imu.hpp"  // Include the IMU message header
+#include "icarus_arm_control/msg/pimu.hpp"
+#include "icarus_arm_control/msg/integrated_angles.hpp"
+
+class ImuPimuListener : public rclcpp::Node
+{
+public:
+    ImuPimuListener();
+    void integrate();
+    void step();
+
+    //const icarus_arm_control::msg::PIMU pimu;
+    //const sensor_msgs::msg::Imu::SharedPtr imu
+
+    icarus_arm_control::msg::IntegratedAngles integrated_angles_;
+
+private:
+    void imu_callback(const sensor_msgs::msg::Imu &imu);
+    void pimu_callback(const icarus_arm_control::msg::PIMU::SharedPtr pimu);
+    
+    // Subscriptions
+    rclcpp::Subscription<icarus_arm_control::msg::PIMU>::SharedPtr sub_pimu_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr sub_imu_;
+
+    //Publishers
+    rclcpp::Publisher<icarus_arm_control::msg::IntegratedAngles>::SharedPtr pub_int_;
+
+    // IMU state
+    double angular_velocity_x_, angular_velocity_y_, angular_velocity_z_;
+    double imu_dt_;
+
+    // Integrated angles
+    double integrated_theta_ = 0.0;
+    double integrated_phi_ = 0.0;
+    double integrated_psi_ = 0.0;
+
+    //Integrated Angles array
+    
+
+    // Previous angles
+    double prev_theta_ = 0.0;
+    double prev_phi_ = 0.0;
+    double prev_psi_ = 0.0;
+};
+
+ 
+#endif
---
+++ icarus-arm-control/src/imu_pimu_listener.cpp
@@ -0,0 +1,65 @@
+
+#include "controller.hpp"
+#include "imu_pimu.hpp"
+
+ImuPimuListener::ImuPimuListener()
+: Node("integrator_node")
+{
+    sub_pimu_= this->create_subscription<icarus_arm_control::msg::PIMU>("pimu", 1, std::bind(&ImuPimuListener::pimu_callback, this, std::placeholders::_1));
+    sub_imu_ = this->create_subscription<sensor_msgs::msg::Imu>("imu", 1, std::bind(&ImuPimuListener::imu_callback, this, std::placeholders::_1));
+    
+    pub_int_ = this->create_publisher<icarus_arm_control::msg::IntegratedAngles>("integrated_angles", 1);
+}
+
+void ImuPimuListener::step(){
+
+    integrate();
+    RCLCPP_INFO(this->get_logger(), "Integrated Angles: [%.4f, %.4f, %.4f]", integrated_theta_, integrated_phi_, integrated_psi_);
+
+}
+
+
+
+void ImuPimuListener::imu_callback(const sensor_msgs::msg::Imu &imu)
+{
+    angular_velocity_x_ = imu.angular_velocity.x;
+    angular_velocity_y_ = imu.angular_velocity.y;
+    angular_velocity_z_ = imu.angular_velocity.z;
+}
+
+void ImuPimuListener::pimu_callback(const icarus_arm_control::msg::PIMU::SharedPtr pimu)
+{
+    imu_dt_ = pimu->dt;
+}
+
+void ImuPimuListener::integrate()
+{
+    integrated_theta_ = prev_theta_ + (imu_dt_ * angular_velocity_x_);
+    integrated_phi_   = prev_phi_   + (imu_dt_ * angular_velocity_y_);
+    integrated_psi_   = prev_psi_   + (imu_dt_ * angular_velocity_z_);
+
+    prev_theta_ = integrated_theta_;
+    prev_phi_   = integrated_phi_;
+    prev_psi_   = integrated_psi_;
+
+    integrated_angles_.integrated_angles = {integrated_theta_,integrated_phi_,integrated_psi_};
+    pub_int_->publish(integrated_angles_);
+}
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ImuPimuListener>();
+
+    rclcpp::Rate loop_rate(10); // 10 Hz
+    while (rclcpp::ok()) {
+        rclcpp::spin_some(node);
+        node->step();
+        loop_rate.sleep();
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
+
+

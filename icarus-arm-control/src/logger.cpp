0a2d5c6ab0713ce836b578b4b1a49adaf77aa062
Winnie Regan <reganwinnifred@gmail.com>
2025-04-08 15:32:08 -0600

    Separated packages, confirm on pi, then replace arm-control branch with this one

---
+++ icarus-arm-control/src/logger.cpp
@@ -0,0 +1,166 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+#include <sstream>
+#include <vector>
+
+#include "controller.hpp"
+
+class LoggerNode : public rclcpp::Node
+{
+public:
+    LoggerNode()
+        : Node("logger_node")
+    {
+        // Open CSV file and write headers
+        file_.open("logged_data.csv", std::ios::out);
+        if (!file_.is_open()) {
+            RCLCPP_ERROR(this->get_logger(), "Failed to open logged_data.csv");
+            return;
+        }
+
+        file_ << "timestamp,motor_commanded_torque,motor_encoder_torque,motor_encoder_torque_target,motor_encoder_angle,motor_encoder_velocity"
+              << "imu_dt,imu_dthetax,imu_dthetay,imu_dthetaz,imu_dvelx,imu_dvely,imu_dvelz,imu_ang_velx,imu_ang_vely,imu_ang_velz,imu_lin_accelx,imu_lin_accely,imu_lin_accelz"
+              << "ins_theta,ins_phi,ins_psi"
+              <<  std::endl;
+        
+        // Motor subscriptions
+        sub_motor_cntr_stat_    = this->create_subscription<icarus_arm_control::msg::ControllerStatus>("controller_status", 1, std::bind(&LoggerNode::cbCtrlStatusData, this, std::placeholders::_1));
+        sub_motor_cntr_msg_     = this->create_subscription<icarus_arm_control::msg::ControlMessage>("control_message", 1, std::bind(&LoggerNode::cbCtrlMessageData, this, std::placeholders::_1));
+
+        // IMU subscriptions
+        sub_pimu_               = this->create_subscription<icarus_arm_control::msg::PIMU>("pimu", 1, std::bind(&LoggerNode::cbPIMUData, this, std::placeholders::_1));
+        sub_imu_                = this->create_subscription<sensor_msgs::msg::Imu>("imu", 1, std::bind(&LoggerNode::cbIMUData, this, std::placeholders::_1));
+        sub_ins_                = this->create_subscription<icarus_arm_control::msg::DIDINS1>("ins_eul_uvw_ned", 1, std::bind(&LoggerNode::cbINSData, this, std::placeholders::_1));
+    }
+
+    ~LoggerNode()
+    {
+        if (file_.is_open()) {
+            file_.close();
+        }
+    }
+
+    void logToCSV(double startTime)
+    {
+        // Get current system time for timestamp
+        currTime = static_cast<double>( current_timeMs() ) - startTime;
+
+        // Write to CSV
+        file_ << currTime << ","
+              << commanded_torque << "," << encoder_torque << "," << encoder_torque_target << "," << encoder_position << "," << encoder_velocity << ","
+              << dt << "," << dtheta_x << "," << dtheta_y << "," << dtheta_z << "," << dvel_x << "," << dvel_y << "," << dvel_z << ","
+              << ang_vel_x << "," << ang_vel_y << "," << ang_vel_z << "," << lin_accel_x << "," << lin_accel_y << "," << lin_accel_z << ","
+              << theta_ins << "," << phi_ins << "," << psi_ins
+              << std::endl;
+    }
+
+    icarus_arm_control::msg::ControlMessage msg_ctrl;
+
+    void cbCtrlStatusData(const icarus_arm_control::msg::ControllerStatus::SharedPtr ctrl_stat)
+    {
+        encoder_position = ctrl_stat->pos_estimate;
+        encoder_velocity = ctrl_stat->vel_estimate;
+        encoder_torque = ctrl_stat->torque_estimate;
+        encoder_torque_target = ctrl_stat->torque_target;
+    }
+
+    void cbCtrlMessageData(const icarus_arm_control::msg::ControlMessage::SharedPtr msg_ctrl)
+    {
+        commanded_torque = msg_ctrl->input_torque;
+    }
+
+    void cbPIMUData(const icarus_arm_control::msg::PIMU::SharedPtr pimu)
+    {
+        dt = pimu->dt;
+
+        // Store (preintegrated) delta theta values into class members
+        dtheta_x = pimu->dtheta.x;
+        dtheta_y = pimu->dtheta.y;
+        dtheta_z = pimu->dtheta.z;
+
+        // Store (preintegrated) delta theta values into class members
+        dvel_x = pimu->dvel.x;
+        dvel_y = pimu->dvel.y;
+        dvel_z = pimu->dvel.z;
+    }
+
+    void cbIMUData(const sensor_msgs::msg::Imu &imu)
+    {
+
+        // Store angular velocity values into class members
+        ang_vel_x = imu.angular_velocity.x;
+        ang_vel_y = imu.angular_velocity.y;
+        ang_vel_z = imu.angular_velocity.z;
+
+        // Store acceleration values into class members
+        lin_accel_x = imu.linear_acceleration.x;
+        lin_accel_y = imu.linear_acceleration.y;
+        lin_accel_z = imu.linear_acceleration.z;
+    }
+
+    void cbINSData(const icarus_arm_control::msg::DIDINS1::SharedPtr did_ins1)
+    {
+        theta_ins = did_ins1->theta[0];
+        phi_ins = did_ins1->theta[1];
+        psi_ins = did_ins1->theta[2];
+    }
+
+    rclcpp::Subscription<icarus_arm_control::msg::PIMU>::SharedPtr sub_pimu_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr sub_imu_;
+    rclcpp::Subscription<icarus_arm_control::msg::DIDINS1>::SharedPtr sub_ins_;
+
+    rclcpp::Subscription<icarus_arm_control::msg::ControlMessage>::SharedPtr sub_motor_cntr_msg_;
+    rclcpp::Subscription<icarus_arm_control::msg::ControllerStatus>::SharedPtr sub_motor_cntr_stat_;
+
+    std::ofstream file_;
+
+    // Timestamp variables
+    double nodeStartTime;
+    double currTime;
+
+    // Variables to store encoder and torque data
+    double encoder_position;
+    double encoder_velocity;
+
+    double commanded_torque;
+    double encoder_torque;
+    double encoder_torque_target;
+
+    // Variables to store imu data
+    double dt;
+
+    double dtheta_x, dtheta_y, dtheta_z;
+    double dvel_x, dvel_y, dvel_z;
+
+    double ang_vel_x, ang_vel_y, ang_vel_z;
+    double lin_accel_x, lin_accel_y, lin_accel_z;
+
+    // Variables to store ins data
+    double theta_ins, phi_ins, psi_ins;
+
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<LoggerNode>();
+
+    // Node Start Time, set on start of main mode activity
+    node->nodeStartTime = static_cast<double>( current_timeMs() );
+
+    rclcpp::Rate rate(10);  // 10 Hz, adjust as needed
+    while (rclcpp::ok()) {
+        rclcpp::spin_some(node);
+        node->logToCSV(node->nodeStartTime);
+        rate.sleep();
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ icarus-arm-control/src/main.cpp
@@ -0,0 +1,96 @@
+ /**
+ * @file main.cpp
+ *
+ * @brief Controller listener node main function, will drive the primary functionality 
+ * of the entire controlling system.
+ *
+ */
+
+#include "controller.hpp"
+
+#define NDEBUG
+
+#ifndef NDEBUG
+#define ASSERT_EX(condition, statement) \
+    do { \
+        if (!(condition)) { statement; assert(condition); } \
+    } while (false)
+#else
+#define ASSERT_EX(condition, statement) ((void)0)
+#endif
+
+ int main(int argc, char**argv)
+ {
+    // Universal rclcpp init
+    rclcpp::init(argc, argv);
+    
+    auto controller_node = std::make_shared<Controller>();
+    // auto can_node = std::make_shared<ODriveCanNode>("ODriveCanNode");
+
+    // Initialize our controller object
+    controller_node->init();
+
+    bool success = false;
+    unsigned int startTimeMs = current_timeMs(), prevTimeMs = 0, nowTimeMs;
+	while((nowTimeMs = current_timeMs()) - startTimeMs < 5000)
+	{
+	    rclcpp::spin_some(controller_node);
+
+        controller_node->step();
+
+        if (controller_node->did_rx_pimu_) {
+            success = true;
+            break;
+        } else {
+            // check regularly, but don't print regularly..
+            SLEEP_MS(14);
+            if (prevTimeMs / 1000 != nowTimeMs / 1000) {
+                RCLCPP_INFO(rclcpp::get_logger("init"),"waiting...  (time: %u)\n", nowTimeMs);
+                prevTimeMs = nowTimeMs;
+                controller_node->print_data();
+            }
+        }
+    }
+
+    ASSERT_EX(success, std::cout << "IMU RX fail.\n");
+
+    controller_node->nodeStartTime = static_cast<double>( current_timeMs() );
+    
+    int data_points = 5000;
+    int i = 0;
+    while (i <= data_points) {
+
+        std::cout << "Calibrating... " << std::to_string(i) << "/" << std::to_string(data_points) << std::endl;
+
+        rclcpp::spin_some(controller_node);
+        controller_node->imu_configure(i);
+        i += 1;
+    }
+
+    rclcpp::spin_some(controller_node);
+    controller_node->bias_calibrate();
+    
+
+    while (ok())
+    {
+        rclcpp::spin_some(controller_node);
+
+        controller_node->step();
+
+        // periodic print, add update function
+        SLEEP_MS(500);
+        if (prevTimeMs / 1000 != nowTimeMs / 1000) {
+            if (!controller_node->plot_quiet) {
+                controller_node->plot(controller_node->nodeStartTime);
+            }
+            RCLCPP_INFO(rclcpp::get_logger("controller"),"running...  (time: %u)\n\n", nowTimeMs);
+            prevTimeMs = nowTimeMs;
+        }
+        nowTimeMs = current_timeMs();
+    }
+
+    rclcpp::shutdown();
+    
+    return 0;
+ }
+ 
